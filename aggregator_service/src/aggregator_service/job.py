70d56537ed9a0785d96f9dc9e4244c7eff90ccf8
zceee12 <richardclose@gmail.com>
2017-06-06 20:42:21 +0100

    Add id field to Job

--- aggregator_service/src/aggregator_service/job.py
+++ aggregator_service/src/aggregator_service/job.py
@@ -1,6 +1,8 @@
 class Job(object):
 
-    def __init__(self, event_id, artists):
+    def __init__(self, id, event_id, artists):
+        self.id = id
         self.status = "pending"
         self.event_id = event_id
         self.artists = artists
+        self.result = None
--- aggregator_service/test/test_job.py
+++ aggregator_service/test/test_job.py
@@ -5,9 +5,11 @@ from aggregator_service.job import Job
 class TestJob(TestCase):
 
     def setUp(self):
-        self.job = Job("123", ["Radiohead", "Ed Sheeran"])
+        self.job = Job("uuid", "123", ["Radiohead", "Ed Sheeran"])
 
     def test_init(self):
+        self.assertEquals("uuid", self.job.id)
         self.assertEquals("123", self.job.event_id)
         self.assertEquals(["Radiohead", "Ed Sheeran"], self.job.artists)
         self.assertEquals("pending", self.job.status)
+        self.assertEquals(None, self.job.result)
